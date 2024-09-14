aa04a3aac19a0611bb5062cd22679ec5272d6fcd
madhava <madhavavukkum@gmail.com>
2024-09-14 18:51:42 +0530

    Fix: removed unused arrayUnion import

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,14 +7,14 @@ import '../styles/Dashboard.css';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFUli-CDl2U0qpOn43uh_tPJvG7pSSr-I",
-  authDomain: "eduswap-42359.firebaseapp.com",
-  databaseURL: "https://eduswap-42359-default-rtdb.firebaseio.com",
-  projectId: "eduswap-42359",
-  storageBucket: "eduswap-42359.appspot.com",
-  messagingSenderId: "745544145688",
-  appId: "1:745544145688:web:31c5c229da14fefad31c6d",
-  measurementId: "G-E5WRBEYCXQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
